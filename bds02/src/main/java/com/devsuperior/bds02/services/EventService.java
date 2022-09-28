0c487af1ee6f231942c395b3c758701bc1b2f91a
brunosilva-dev <silva.dev@hotmail.com>
2022-09-28 17:18:52 -0300

    Subindo tarefa TDD

---
+++ bds02/src/main/java/com/devsuperior/bds02/services/EventService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.bds02.services;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.EventDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.entities.Event;
+import com.devsuperior.bds02.repositories.EventRepository;
+import com.devsuperior.bds02.services.resources.ResourceNotFoundException;
+
+@Service
+public class EventService {
+
+	@Autowired
+	private EventRepository repository;
+
+	@Transactional
+	public Page<EventDTO> findAll(Pageable pageable) {
+		Page<Event> page = repository.findAll(pageable);
+		return page.map(x -> new EventDTO(x));
+	}
+	
+	@Transactional
+	public EventDTO update(Long id, EventDTO dto) {
+
+		try {
+			Event entity = repository.getOne(id);
+			entity.setName(dto.getName());
+			entity.setDate(dto.getDate());
+			entity.setUrl(dto.getUrl());
+			entity.setCity(new City(dto.getCityId(),null));
+			entity = repository.save(entity);
+			return new EventDTO(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+	}
+
+}
